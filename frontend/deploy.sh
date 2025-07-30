6276149ce07a4b61c63a7b96b6d96789e73acff9
SeorangManusia99 <hokiabis88@gmail.com>
2025-07-30 16:51:30 +0800

    Create deploy.sh

---
+++ frontend/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Hentikan proses jika terjadi error
+set -e
+
+# Build proyek (membuat folder 'dist')
+npm run build
+
+# Masuk ke folder hasil build
+cd dist
+
+# Inisialisasi git baru di dalam folder dist dan commit
+git init
+git add -A
+git commit -m 'deploy'
+
+# Push secara paksa ke branch gh-pages di repository Anda
+git push -f git@github.com:SeorangManusia99/dana-bulanan-app.git main:gh-pages
+
+cd -
